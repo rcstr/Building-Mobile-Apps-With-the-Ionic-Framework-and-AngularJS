bc4c7250a4cb9fc5a9ad12a94fe2b0e2a5c00f11
Rommel Castro <rommelxcastro@gmail.com>
2015-08-30 20:40:45 -0600

    team detail ctrl and page, teams basic stuff

---
+++ EliteScheduleApp/www/app/team/team-detail-ctrl.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular.module('eliteApp').controller('teamDetailCtrl', ['$stateParams',teamDetailCtrl]);
+
+  function teamDetailCtrl($stateParams) {
+    var vm = this;
+
+    console.log('$stateParams', $stateParams);
+  };
+})();
